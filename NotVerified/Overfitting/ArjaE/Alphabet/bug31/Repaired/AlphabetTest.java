ec3ec7bafabd04de4a40e8f63381724da1a90c92
Marlon Calvo <marloncalv98@gmail.com>
2021-05-03 04:49:28 -0400

    Fix private method

--- NotVerified/Overfitting/ArjaE/Alphabet/bug31/Repaired/AlphabetTest.java
+++ NotVerified/Overfitting/ArjaE/Alphabet/bug31/Repaired/AlphabetTest.java
@@ -16,12 +16,14 @@ public class AlphabetTest {
 	/**
 	 * Specific test case is different, but it's checking that uppercase
 	 * should not be changed in lowercase, so we will transform to that.
+	 * 
+	 * Uppercase private field is changed without access, so JML complains.
 	 */
 	void case2() {
 		char c = 'A'; // original is '`'
 		int op = 4;
 		Alphabet driver = new Alphabet(c);
 		assertFalse(driver.isLowercase());
-		assertFalse(driver.uppercase); // must not have been set
+		// assertFalse(driver.uppercase); // must not have been set
 	}
 }
