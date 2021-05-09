2ab9d50b1e920c31b164277350847752713a0983
Marlon Calvo <marloncalv98@gmail.com>
2021-05-09 11:54:17 -0400

    Add transpose matrix test case

---
+++ NotVerified/Overfitting/ArjaE/TransposeMatrix/bug4/Repaired/TransposeMatrixTest.java
@@ -0,0 +1,21 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+public class TransposeMatrixTest {
+    
+    /**
+     * Input array too large.
+     */
+    @Test
+    void test() {
+		fail("The function transposeMat() is not testable for this case");
+    }
+
+    /**
+     * Input array too large.
+     */
+    @Test
+    void test0() {
+		fail("The function transposeMat() is not testable for this case");
+    }
+
+}
